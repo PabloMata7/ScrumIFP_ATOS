b55025e05e73c9e3de0f4818a08c4c555d10bc6f
Diego Díaz <diegodiazhellinl@outlook.com>
2026-03-05 13:29:55 +0100

    arreglojs

--- ProyectoRecetario/scripts.js
+++ ProyectoRecetario/scripts.js
@@ -1,41 +1,43 @@
-fetch("recetas.json")
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        const contenedor = document.getElementById("contenedor-tarjetas");
-        
-        let htmlAcumulado = "";
-
-        datos.recetas.forEach(receta => {
+document.addEventListener('DOMContentLoaded', function() {
+    fetch("recetas.json")
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            const contenedor = document.getElementById("contenedor-tarjetas");
             
-            let ingredientesHTML = "";
-            receta.ingredientes.forEach(ing => {
-                ingredientesHTML += `<li>${ing}</li>`;
+            let htmlAcumulado = "";
+
+            datos.recetas.forEach(receta => {
+                
+                let ingredientesHTML = "";
+                receta.ingredientes.forEach(ing => {
+                    ingredientesHTML += `<li>${ing}</li>`;
+                });
+
+                htmlAcumulado += `
+                    <div class="tarjeta">
+                        <div class="tarjeta-img">Img</div>
+                        <h3>${receta.nombre}</h3>
+                        <p>${receta.descripcion}</p>
+                        
+                        <div class="tarjeta-ingredientes">
+                            <strong>Ingredientes:</strong>
+                            <ul>
+                                ${ingredientesHTML}
+                            </ul>
+                        </div>
+
+                        <div class="botones-tarjeta">
+                            <button class="btn-ver-mas">Ver más</button>
+                            <button class="btn-fav">🤍</button>
+                        </div>
+                    </div>
+                `;
             });
 
-            htmlAcumulado += `
-                <div class="tarjeta">
-                    <div class="tarjeta-img">Img</div>
-                    <h3>${receta.nombre}</h3>
-                    <p>${receta.descripcion}</p>
-                    
-                    <div class="tarjeta-ingredientes">
-                        <strong>Ingredientes:</strong>
-                        <ul>
-                            ${ingredientesHTML}
-                        </ul>
-                    </div>
+            contenedor.innerHTML = htmlAcumulado;
 
-                    <div class="botones-tarjeta">
-                        <button class="btn-ver-mas">Ver más</button>
-                        <button class="btn-fav">🤍</button>
-                    </div>
-                </div>
-            `;
+        })
+        .catch(error => {
+            console.error("Error crítico al cargar el JSON:", error);
         });
-
-        contenedor.innerHTML = htmlAcumulado;
-
-    })
-    .catch(error => {
-        console.error("Error crítico al cargar el JSON:", error);
-    });
+});
